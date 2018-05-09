8cb48faa7878097dc3f9575e23d421ef80d0ab08
marciusOrigenes <marcius_servo@hotmail.com>
2018-05-09 17:12:24 -0300

    Add files via upload

--- src/frontend/InterfaceController.java
+++ src/frontend/InterfaceController.java
@@ -2,7 +2,9 @@ package frontend;
 
 import backend.QueryController.IQueryController;
 import frontend.Screens.FirstScreen;
+import frontend.Screens.ForthScreen;
 import frontend.Screens.SecondScreen;
+import frontend.Screens.ThirdScreen;
 
 public abstract class InterfaceController {
 
@@ -11,10 +13,18 @@ public abstract class InterfaceController {
     public static void init(IQueryController _controller) {
         controller = _controller;
         new FirstScreen(controller);
+       
     }
 
     public static void startSecondScreen() {
         new SecondScreen(controller);
+    }
+    
+    public static void startThirdScreen() {
+        new ThirdScreen(controller);
+    }
+    public static void startForthScreen() {
+        new ForthScreen(controller);
     }
 
 }
