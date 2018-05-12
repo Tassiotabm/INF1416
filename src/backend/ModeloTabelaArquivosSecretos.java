2464b2a0d9019ae33fa211a2392f6b6a9d006821
marciusOrigenes <marcius_servo@hotmail.com>
2018-05-12 17:31:31 -0300

    Add files via upload

---
+++ src/backend/ModeloTabelaArquivosSecretos.java
@@ -0,0 +1,57 @@
+package backend;
+
+import java.util.ArrayList;
+
+import javax.swing.table.AbstractTableModel;
+
+public class ModeloTabelaArquivosSecretos extends AbstractTableModel {
+	private ArrayList linhas=null;
+	private String[] colunas=null;
+	
+	
+	public ModeloTabelaArquivosSecretos(ArrayList lin, String[] col) {
+		setLinhas(lin);
+		setColunas(col);
+	}
+	
+	
+	@Override
+	public int getColumnCount() {
+		
+		return colunas.length;
+	}
+	public ArrayList getLinhas() {
+		return linhas;
+	}
+
+
+	public void setLinhas(ArrayList linhas) {
+		this.linhas = linhas;
+	}
+
+
+	public String[] getColunas() {
+		return colunas;
+	}
+
+
+	public void setColunas(String[] colunas) {
+		this.colunas = colunas;
+	}
+
+
+	@Override
+	public int getRowCount() {
+	
+		return linhas.size();
+	}
+	@Override
+	public Object getValueAt(int numLin, int numCol) {
+		Object[] linha=(Object[])getLinhas().get(numLin);
+		return linha[numCol];
+	}
+	
+	public String getColumnName(int numCol) {
+		return colunas[numCol];
+	}
+}
